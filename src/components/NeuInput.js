56978c6557fdbfb60e4b6737df2344682fc4e81a
dev-saeed <saeed_dev@yahoo.com>
2019-12-31 19:52:13 +0500

    improved styling of NeuInput component

--- src/components/NeuInput.js
+++ src/components/NeuInput.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 var neuCardStyles = {
-    borderRadius: '7px solid white',
+    borderRadius: '15px',
+    outline:'0',
     width: '100%',
     height: '20px',
     padding: '5px',
-    backgroundColor: '#E0E5EC',
-    boxShadow: '9px 9px 16px rgb(163,177,198,0.6), -9px -9px 16px rgba(255,255,255, 0.5)'
+    border: '5px solid #e2edf7',
+    backgroundColor: 'rgb(224, 229, 236,0.6)',
+    boxShadow: '9px 9px 16px rgb(163,177,198,0.6), -9px -9px 16px rgba(255,255,255, 0.5)',
+    boxShadow: 'inset  -9px -9px 16px rgb(163,177,198, 0.2), 9px 9px 16px rgb(163,177,198,0.6)'
 }
 
 export class NeuInput extends Component {
