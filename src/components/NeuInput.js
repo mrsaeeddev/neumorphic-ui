95ba90e83de7b5c12b39e70d7276139519287e7a
sa-js <philipthevictor@gmail.com>
2020-01-11 10:44:46 +0500

    redesigned NeuInput component

--- src/components/NeuInput.js
+++ src/components/NeuInput.js
@@ -7,10 +7,9 @@ var neuInputStyles = {
     width: '100%',
     height: '20px',
     padding: '5px',
-    border: '5px solid #e2edf7',
+    border: '5px solid #e8eef7',
     backgroundColor: 'rgb(224, 229, 236,0.6)',
-    boxShadow: '9px 9px 16px rgb(163,177,198,0.6), -9px -9px 16px rgba(255,255,255, 0.5)',
-    boxShadow: 'inset  9px 9px 9px rgb(163,177,198, 0.2), 9px 9px 16px rgb(163,177,198,0.6)'
+    boxShadow: 'inset 3px 3px 3px rgba(163,177,198, 0.7),inset -3px -3px 4px rgba(255, 255, 255, 0.5), 9px 9px 16px rgb(163,177,198,0.6)'
 }
 
 export class NeuInput extends Component {
